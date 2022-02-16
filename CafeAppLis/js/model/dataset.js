41563256e51dc281233aa87553d6ab3062442af7
alisonsandrade <alison.sandrade@hotmail.com>
2022-02-16 00:06:40 -0300

    Add campo price para exibição nos card's

--- CafeAppLis/js/model/dataset.js
+++ CafeAppLis/js/model/dataset.js
@@ -9,6 +9,7 @@ const dataset = [
       id: 1,
       name: 'Hambúrguer',
       image: 'https://www.sabornamesa.com.br/media/k2/items/cache/b9ad772005653afce4d4bd46c2efe842_XL.jpg',
+      price: 9.50,
       description:
         'Um hambúrguer é um sanduíche que consiste em um ou mais hambúrgueres cozidos de carne moída, geralmente carne bovina, colocados dentro de um pão ou pão fatiado. O hambúrguer pode ser frito, grelhado, defumado ou grelhado',
     },
@@ -16,6 +17,7 @@ const dataset = [
       id: 2,
       name: 'Sanduíche',
       image: 'images/sanduiche.jpg',
+      price: 7.80,
       description:
         'O sanduíche (do inglês sandwich), também popularmente chamado de sanduba (português brasileiro) ou sandes (português europeu), é um tipo de alimento que consiste em duas fatias de um pão inteiro, entre as quais é colocada carne, queijo ou outro tipo de alimento.Os sanduíches são, habitualmente, consumidos ao lanche ou como uma refeição rápida, durante o almoço ou o jantar.',
     },
@@ -23,6 +25,7 @@ const dataset = [
       id: 3,
       name: 'Café',
       image: 'images/cafe.jpg',
+      price: 4.75,
       description:
         'O café é uma bebida produzida a partir dos grãos torrados do fruto do cafeeiro. É servido tradicionalmente quente, mas também pode ser consumido gelado. O café é um estimulante, por possuir cafeína — geralmente 80 a 140 mg para cada 207 ml dependendo do método de preparação.',
     },
@@ -30,6 +33,7 @@ const dataset = [
       id: 4,
       name: 'Suco',
       image: 'images/suco.jpg',
+      price: 6.50,
       description:
         'Um suco (português brasileiro) ou sumo (português europeu) é uma bebida produzida do líquido extraído de frutos. Além dos frutos, sucos também podem ser obtidos pelo processamento de outras partes dos vegetais, tais como folhas, talos ou raízes.',
     },
